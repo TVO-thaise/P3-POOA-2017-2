6423be953af54029c5607a4f055def5f4ee557a1
TVO-thaise <thaisevilaca2013@gmail.com>
2018-03-26 14:32:29 -0300

    Configurando o Realm

---
+++ app/src/main/java/com/example/samsung/p3_pooa_2017_2/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.samsung.p3_pooa_2017_2.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.samsung.p3_pooa_2017_2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
